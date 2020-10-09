4ec31f3cc58b1bb89ceaf7c3841bf9124c782b86
Issamoxix <issamhaidaoui@gmail.com>
2020-10-09 20:34:58 +0100

    api wrapper

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import summ , sys
-data_ = summ.argparser()
-data = vars(data_)
-Summoner = summ.summoner(data['u'],data['r'],data['k'])
-Summoner.res()
+# data_ = summ.argparser()
+# data = vars(data_)
+# Summoner = summ.summoner(data['u'],data['r'],data['k'])
+# Summoner.res()
+# print(Summoner.accountId)
+league_api = summ.summoner('Summoner name','euw','api_key')
+
+solo_q=league_api.rank_data
+league_api.res()
--- ur.py
+++ ur.py
@@ -3,24 +3,32 @@ class requestss:
     def __init__(self,key,name,reg):
         self.key = key
         self.name = name
+        self.reg = False
         self.req = reg
+        self.error = False
+        self.reg_tp= {'euw':'euw1','na':'na1'}
+        typee = None
+        self.url = "https://"+str(self.req)+".api.riotgames.com/lol"+str(typee)+"?api_key="+str(self.key)
     #get account infos lvl , sumid , etc..
     def get_account_data(self):
-        if self.req =='EUW' or self.req == "euw":
-            reg = 'euw1'
-            self.reg = reg
-        elif self.req=='NA' or self.req =="na":
-            reg = 'na1'
-            self.reg = reg
-        else: 
-            return False
+        for i in self.reg_tp:
+            if self.req.lower() == i:
+                self.reg = self.reg_tp[i]
+        if not self.reg:
+            self.error = 'Error : The region you have chosen is wrong'
+            return self.error
         typee="/summoner/v4/summoners/by-name/"
-        url = "https://"+reg+".api.riotgames.com/lol"+typee+self.name+"?api_key="+self.key
+        url = "https://"+str(self.reg)+".api.riotgames.com/lol"+str(typee)+str(self.name)+"?api_key="+str(self.key)
         x = req.get(url)
         return x.json()
     #get infos about ranke
     def get_rank(self,s_id):
         typee="/league/v4/entries/by-summoner/"
         url = "https://"+self.reg+".api.riotgames.com/lol"+typee+s_id+"?api_key="+self.key
+        x = req.get(url)
+        return x.json()
+    def get_match_history(self,s_id):
+        typee ="/match/v4/matchlists/by-account/"
+        url ="https://"+self.reg+".api.riotgames.com/lol"+typee+s_id+"?api_key="+self.key
         x = req.get(url)
         return x.json()
